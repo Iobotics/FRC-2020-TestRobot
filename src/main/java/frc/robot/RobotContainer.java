db732951bbca4030004d409994080b8b1fda6884
Shane Matsushima <skm1901@iolani.org>
2020-01-09 16:17:43 -1000

    Added Vision branch and limelight subsystem with get methods

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,9 +10,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Limelight;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 
@@ -25,7 +27,7 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final Drivetrain drivetrain = new Drivetrain();
-
+  private final Limelight limelight = new Limelight();
   private final Joystick joystick1 = new Joystick(Constants.kJoystick1);
   private final Joystick joystick2 = new Joystick(Constants.kJoystick2);
 
@@ -40,6 +42,10 @@ public class RobotContainer {
       (new RunCommand(() -> drivetrain
         .setTank(joystick1.getY(), 
           joystick2.getY()), drivetrain));
+
+    limelight.setDefaultCommand
+    (new RunCommand(() -> limelight.printValues(), limelight));
+    
   }
 
   /**
