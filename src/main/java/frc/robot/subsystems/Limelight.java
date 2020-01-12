b53017661ee1b6d273bddc3d9dfe58930022e1cf
TheKiller65YT <jackgonser@gmail.com>
2020-01-11 14:29:10 -1000

    Added getArea function for testing in limelight

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -20,26 +20,60 @@ public class Limelight extends SubsystemBase {
   /**
    * Creates a new Limelight.
    */
-  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
-  NetworkTableEntry tx = table.getEntry("tx");
-  NetworkTableEntry ty = table.getEntry("ty");
-  NetworkTableEntry ta = table.getEntry("ta");
+  private  NetworkTable table; 
+  private  NetworkTableInstance inst;
+  private  NetworkTableEntry tv;
+  private  NetworkTableEntry tx;
+  private  NetworkTableEntry ty;
+  private  NetworkTableEntry ta;
+  private  NetworkTableEntry ledMode;
 
-  double x;
-  double y;
-  double area;
+  private  boolean isDetected;
+  private  double x = 0;
+  private  double y = 0;
+  private  double area = 0;
+
+
+
+
+
+
 
   public Limelight()  {
      
   }
 
   public void printValues(){
+
+    
+  NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  tx = table.getEntry("tx");
+  ty = table.getEntry("ty");
+  ta = table.getEntry("ta");
+  tv = table.getEntry("tv");
+
     x = tx.getDouble(0.0);
     y = ty.getDouble(0.0);
     area = ta.getDouble(0.0);
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimeilightY", y);
     SmartDashboard.putNumber("LimeLightArea", area);
+  }
+
+  public boolean isTargetDetected()
+  {
+    if(tv.getDouble(0.0) == 1)
+    { 
+      return true;
+    }
+    return false;
+  }
+  public double getDistance()
+  {
+    double area = ta.getDouble(0.0);
+    double distance; // mathwork needed base on height of bot and angle of camera
+    return area;
+
   }
   public double getTX(){
     x = tx.getDouble(0.0);
@@ -49,11 +83,6 @@ public class Limelight extends SubsystemBase {
   public double getTY(){
     y = ty.getDouble(0.0);
     return y;
-  }
-
-  public double getTA(){
-    area = ta.getDouble(0.0);
-    return area;
   }
 
   @Override
