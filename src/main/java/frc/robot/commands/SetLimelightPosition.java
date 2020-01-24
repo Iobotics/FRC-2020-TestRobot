c86d472ec9fa336bd2a3c60e92062f78c2691970
TheKiller65YT <jackgonser@gmail.com>
2020-01-23 17:28:09 -1000

    fixed the limelight servo command to just go back and forth on button press..started the taking of inputs from the SmartDashboard

--- src/main/java/frc/robot/commands/SetLimelightPosition.java
+++ src/main/java/frc/robot/commands/SetLimelightPosition.java
@@ -37,32 +37,24 @@ public class SetLimelightPosition extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    position = servo.getServoValue();
+    //position = servo.getServoValue();
+    SmartDashboard.putNumber("distance", 0.0);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {//this is looping until completion
+    //position = servo.getServoValue();\
+    position = SmartDashboard.getNumber("distance", 0.0);
     x = limelight.getTX(); //start by getting the x value from the limelight and saving it to a local variable
     isTarget = limelight.isTargetDetected(); //now your getting whether or not you have a target acquired
-
-    if(isTarget && x > 0){ //checking if there is a target and then if the X value is greater than 0
-      dposition = -1; //set the current position one less
-    }else if(isTarget && x < 0){ //if its on target and is less than 0
-      dposition = 1; //set the position one more
-    }else if(!isTarget){ //if its not on target at all
-      if(position == 1.0){ //if the position is already at max
-        dposition = -1; //move it left
-        RightReached = true; //mark rightside has been searched
-
-      }else{ //this is going to cause a problem of the servo moving back and forth, I may have to add a boolean
-        dposition = +1; //move it right
-        if(position == 0.0){
-          LeftReached = true; //mark leftside has been searched
-        }
-      }
-    }
-    servo.setLimelight(position + dposition*vposition); //set the limelight position to the origin position plus the change of the position * scaler 
+    /*if(position == 1.0){
+      servo.setLimelight(0.0);
+    }else if(position == 0.0){
+      servo.setLimelight(1.0);
+    }*/
+    servo.setLimelight(position);
+    //servo.setLimelight(position + dposition*vposition); //set the limelight position to the origin position plus the change of the position * scaler 
     SmartDashboard.putNumber("servoPosition", servo.getServoValue());
     SmartDashboard.putBoolean("isTargeted", isTarget);
   }
@@ -75,16 +67,12 @@ public class SetLimelightPosition extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if(isTarget && x==0){ //checks whether or not the limelight is centered
-      return true;
-    }
+    //if(isTarget && x==0){ //checks whether or not the limelight is centered
+      //return true;
+    //}
     
     //else if(servo.getServoValue()< 1.1*vposition || servo.getServoValue()> 8.9*vposition) { // check whether or not the servo reach its range of turning
-
-      else if(LeftReached && RightReached){ // check whether or not the servo reach its range of turning(whether both sides have been searched)
-        return true;
-      }
-      return false;
+      return true;
     }
   }
 
